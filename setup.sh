dd473f3298f32ee15b89975c819df04b1fb51276
Rodrigo Feijolo <r.feijolo@gmail.com>
2019-08-17 01:08:27 -0300

    Update docker-compose

--- setup.sh
+++ setup.sh
@@ -72,7 +72,7 @@ function install_docker {
   sudo apt-get install docker-ce -y
   sudo gpasswd -a $USER docker
   newgrp docker
-  sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 }
 
